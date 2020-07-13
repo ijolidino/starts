bddff3ee40eebf47613884c56c2ee7a044c21064
ijolidino <31976195+ijolidino@users.noreply.github.com>
2020-07-13 13:30:22 +0800

    創建了第一個分支

--- SSMManagement/src/main/java/com/blackmagic/helloGit.java
+++ SSMManagement/src/main/java/com/blackmagic/helloGit.java
@@ -3,5 +3,9 @@ package com.blackmagic;
 public class helloGit {
     public static void main(String[] args) {
         System.out.println("hello got!");
+        System.out.println("hello git");
+        System.out.println("the best");
+        System.out.println("您好");
+        System.out.println("創建分支");
     }
 }
